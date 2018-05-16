70528b6bdc442121aad8db77efcc66cc70da7e2b
Kakarpiu <marcinkarpiesiuk@gmail.com>
2018-05-16 12:00:58 +0200

    createPrediction method in Bet created

--- src/Bet.java
+++ src/Bet.java
@@ -1,10 +1,12 @@
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 
 public class Bet {
 
     private int id;
     private Match match;
     private User better;
+    private TeamScore homePrediction = null;
+    private TeamScore awayPrediction = null;
 
     public Bet(int id, Match match, User better){
         this.id = id;
@@ -17,5 +19,12 @@ public class Bet {
         if(!(obj instanceof Bet)) return false;
         Bet other = (Bet) obj;
         return this.better == other.better && this.match == other.match;
+    }
+
+    public void createPrediction(int homeScore, int awayScore){
+        if(homePrediction == null ) homePrediction = new TeamScore(match.getHome().getTeam(), homeScore);
+        else { homePrediction.setScore(homeScore); }
+        if(awayPrediction == null ) awayPrediction = new TeamScore(match.getHome().getTeam(), awayScore);
+        else { awayPrediction.setScore(awayScore); }
     }
 }
